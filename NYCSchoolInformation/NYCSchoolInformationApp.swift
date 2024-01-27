8cdafc0f35d89864bda7d87e7caee5c242d3ac18
khushiv0811 <65448570+khushiv0811@users.noreply.github.com>
2024-01-27 11:09:42 -0500

    Initial Commit

---
+++ NYCSchoolInformation/NYCSchoolInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCSchoolInformationApp.swift
+//  NYCSchoolInformation
+//
+//  Created by Kushboo Patel on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct NYCSchoolInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
